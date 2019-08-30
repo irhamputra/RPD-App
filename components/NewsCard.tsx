fab6d14256501e8b53e406f73e01e7cdb7d9707d
muhamad <muhamad@future.rent>
2019-08-30 15:18:12 +0700

    init commit

---
+++ components/NewsCard.tsx
@@ -0,0 +1,13 @@
+import React from "react";
+import { View } from "react-native";
+import { Text, Card } from "react-native-elements";
+
+const NewsCard: React.FC = () => {
+  return (
+    <View>
+      <Text>News Field</Text>
+    </View>
+  )
+};
+
+export default NewsCard

8a30fff6115f6eba33cf49928b5e2eff2410822b
muhamad <muhamad@future.rent>
2019-08-31 21:01:05 +0700

    overlay component added while loading

--- screens/Chat.tsx
+++ screens/Chat.tsx
@@ -1,8 +1,33 @@
 import React from "react";
 import { WebView } from "react-native-webview";
+import { Overlay, Text } from "react-native-elements";
+import { ActivityIndicator, View } from "react-native";
 
 const Chat = () => {
-  return <WebView source={{ uri: "http://radioppiduniachat.chatango.com/" }} />;
+  const [loading, setLoading] = React.useState(true);
+
+  console.log(loading);
+  return (
+    <>
+      <Overlay overlayStyle={{ height: 200, width: 200}} isVisible={loading}>
+        <View
+          style={{ flex: 1, alignItems: "center", justifyContent: "center" }}
+        >
+          <ActivityIndicator/>
+          <Text style={{ marginTop: 10, fontWeight: 'bold' }}>Loading to Chatbox...</Text>
+        </View>
+      </Overlay>
+      <WebView
+        cacheEnabled
+        onNavigationStateChange={state => {
+          if (!state.loading) {
+            setLoading(state.loading);
+          }
+        }}
+        source={{ uri: "http://radioppiduniachat.chatango.com/" }}
+      />
+    </>
+  );
 };
 
 export default Chat;
